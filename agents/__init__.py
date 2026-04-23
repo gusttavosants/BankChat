c466bdca102b6f81357711b71023b6734fa59e52
gusttavosants <gustavoluizsantosmelo@gmail.com>
2026-04-22 21:03:27 -0300

    chore(agents): configure agents init

---
+++ agents/__init__.py
@@ -0,0 +1,11 @@
+from .triagem import agente_triagem_node
+from .credito import agente_credito_node
+from .entrevista import agente_entrevista_node
+from .cambio import agente_cambio_node
+
+__all__ = [
+    "agente_triagem_node",
+    "agente_credito_node",
+    "agente_entrevista_node",
+    "agente_cambio_node"
+]
