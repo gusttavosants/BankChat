de8d0f5290535a4909ab2ee8caf5fe1a2f9d77f5
gusttavosants <gustavoluizsantosmelo@gmail.com>
2026-04-22 21:03:24 -0300

    chore(services): configure services init

---
+++ services/__init__.py
@@ -0,0 +1,6 @@
+from .auth_service import AuthService
+from .credito_service import CreditoService
+from .score_service import ScoreService
+from .cambio_service import CambioService
+
+__all__ = ["AuthService", "CreditoService", "ScoreService", "CambioService"]
