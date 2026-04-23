9cb2d233a5692cf16797d2a827811fc098efe092
gusttavosants <gustavoluizsantosmelo@gmail.com>
2026-04-22 21:03:23 -0300

    chore(repositories): configure repositories init

---
+++ repositories/__init__.py
@@ -0,0 +1,5 @@
+from .clientes_repository import ClientesRepository
+from .score_repository import ScoreRepository
+from .solicitacoes_repository import SolicitacoesRepository
+
+__all__ = ["ClientesRepository", "ScoreRepository", "SolicitacoesRepository"]
